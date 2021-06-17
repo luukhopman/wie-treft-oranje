b7ccc7409145cc8c7d41217368a996d67f7682da
Luuk Hopman <luukhopman96@outlook.com>
2021-06-17 13:36:26 +0200

    Error handling

--- webapp/errors/handlers.py
+++ webapp/errors/handlers.py
@@ -3,14 +3,14 @@ from flask import Blueprint, render_template
 errors = Blueprint('errors', __name__)
 
 
+@errors.app_errorhandler(400)
+def error_400(error):
+    return render_template('errors/400.html'), 400
+
+
 @errors.app_errorhandler(404)
 def error_404(error):
     return render_template('errors/404.html'), 404
-
-
-@errors.app_errorhandler(403)
-def error_403(error):
-    return render_template('errors/403.html'), 403
 
 
 @errors.app_errorhandler(500)
