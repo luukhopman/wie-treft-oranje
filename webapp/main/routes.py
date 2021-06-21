2b469c017339ecdf9becaa5b24f2c812c2241358
Luuk Hopman <luukhopman96@outlook.com>
2021-06-21 14:31:42 +0200

    Improved logic

--- webapp/main/routes.py
+++ webapp/main/routes.py
@@ -1,4 +1,5 @@
 from flask import render_template, Blueprint, request
+import pandas as pd
 from webapp.main.forms import SenarioTester
 from webapp.main.utils import (get_country,
                                get_current_information,
@@ -16,21 +17,9 @@ def current():
     if request.method == 'POST' and form.validate():
         qualified = form.data['qualified']
         unqualified = form.data['unqualified']
+        pos = int(form.data['position'][0])
 
-        if form.data['position'].startswith('1'):
-            result = get_opposition(qualified, unqualified, pos=1)
-
-        if form.data['position'].startswith('2'):
-            result = f"Oranje moet in de achtste finale tegen de groepswinaar van Groep A. Dat is op dit moment {get_country('1A')}!"
-
-        if form.data['position'] .startswith('3'):
-            if ('C' in qualified) or (len(qualified) < 4):
-                result = get_opposition(qualified, unqualified, pos=3)
-            else:
-                result = 'Uitgeschakeld.'
-
-        if form.data['position'].startswith('4'):
-            result = 'Uitgeschakeld.'
+        result = get_opposition(qualified, unqualified, pos=pos)
 
         return render_template('index.html',
                                position=position,
--- webapp/main/utils.py
+++ webapp/main/utils.py
@@ -15,75 +15,31 @@ OPPONENTS_1ST = ['3D', '3E', '3F', '3E', '3F', '3F', '3D', '3D',
 OPPONENTS_3RD = ['1F', '1F', '1F', 'X', 'X', 'X', '1E', '1E',
                  '1E', 'X', '1F', '1E', '1E', 'X', '1F']
 
-# Scrape all Wikipedia tables
-TABLES = pd.read_html(
-    'https://nl.wikipedia.org/wiki/Europees_kampioenschap_voetbal_2020'
-)
-
-LAST_SCRAPE = datetime.now()
-
 # Group standings table mapping to Wikipedia table number
 GROUP_MAPPING = {'A': 12, 'B': 19, 'C': 26, 'D': 33, 'E': 40, 'F': 47}
 
 
 def refresh_data(first=False):
+    '''
+    Scrapes data from wikipedia every 120 seconds
+    '''
     global LAST_SCRAPE
     global TABLES
 
     # Scrape all Wikipedia tables
-    if (((datetime.now()-LAST_SCRAPE).seconds > 60) or first):
+    if (((datetime.now()-LAST_SCRAPE).seconds > 120) or first):
         TABLES = pd.read_html(
             'https://nl.wikipedia.org/wiki/Europees_kampioenschap_voetbal_2020'
         )
         LAST_SCRAPE = datetime.now()
 
 
+LAST_SCRAPE = datetime.now()
 refresh_data(first=True)
 
 
-def convert_to_text(opponent):
-    if opponent == 'X':
-        return "Uitgeschakeld"
-    else:
-        position, group = opponent[0], opponent[1]
-        if int(position) == 1:
-            return (f"Winnaar van Group {group} "
-                    f"(nu {get_country(opponent)})")
-        else:
-            return (f"{position}e van Group {group} "
-                    f"(nu {get_country(opponent)})")
-
-
-def get_opposition(qualified, unqualified, pos=1):
-    opponent_list = []
-
-    if pos == 1:
-        opponents = OPPONENTS_1ST
-    elif pos == 3:
-        opponents = OPPONENTS_3RD
-
-    for third_places, opponent in zip(THIRD_PLACE_COMBINATIONS, opponents):
-        if (all(i in third_places for i in qualified)
-                and not any(i in third_places for i in unqualified)):
-            opponent_list.append(convert_to_text(opponent))
-
-    opponent_counter = dict(Counter(opponent_list))
-    total = sum(opponent_counter.values())
-    for key in opponent_counter:
-        opponent_counter[key] /= total
-
-    df = pd.DataFrame.from_dict(data=opponent_counter, orient='index')
-    df.columns = ['Kans']
-    df = df.sort_values('Kans', ascending=False)
-    return df.to_html(formatters={'Kans': '{:,.1%}'.format},
-                      classes=["table table-hover text-center"],
-                      table_id="standings",
-                      border=0,
-                      justify="center",
-                      escape=False)
-
-
 def get_position(group, country):
+    '''Returns a country's position in the group'''
     group_table = TABLES[GROUP_MAPPING[group]]
     return (group_table
             .loc[group_table['Team'].str.contains(country), 'Pos']
@@ -91,6 +47,7 @@ def get_position(group, country):
 
 
 def get_country(identifier):
+    '''Returns the country name related to a position in a group'''
     pos, group = int(identifier[0]), identifier[1]
     country = TABLES[GROUP_MAPPING[group]].loc[pos-1, 'Team']
     return (country
@@ -130,3 +87,57 @@ def get_current_information():
         opponent = 'Uitgeschakeld'
 
     return position, best_third_places, opponent
+
+
+def convert_to_text(opponent):
+    '''
+    Converts the position-group identifier to a country name
+    E.g. `1A` -> Italy
+    '''
+    if opponent == 'X':
+        return "Uitgeschakeld"
+    else:
+        position, group = opponent[0], opponent[1]
+        if int(position) == 1:
+            return (f"Winnaar van Group {group} "
+                    f"(nu {get_country(opponent)})")
+        else:
+            return (f"{position}e van Group {group} "
+                    f"(nu {get_country(opponent)})")
+
+
+def get_opposition(qualified, unqualified, pos):
+    opponent_list = []
+
+    # Set possible opponents based on position
+    if pos == 1:
+        opponents = OPPONENTS_1ST
+    elif pos == 2:
+        opponents = ['1A']
+    elif pos == 3:
+        opponents = OPPONENTS_3RD
+    elif pos == 4:
+        opponents = ['X']
+
+    if len(opponents) > 1:
+        for third_places, opponent in zip(THIRD_PLACE_COMBINATIONS, opponents):
+            if (all(i in third_places for i in qualified)
+                    and not any(i in third_places for i in unqualified)):
+                opponent_list.append(convert_to_text(opponent))
+    else:
+        opponent_list.append(convert_to_text(opponents[0]))
+
+    # Opponent probability based on combinations
+    opponent_counter = dict(Counter(opponent_list))
+    total = sum(opponent_counter.values())
+    for key in opponent_counter:
+        opponent_counter[key] /= total
+
+    df = pd.DataFrame.from_dict(data=opponent_counter, orient='index')
+    df.columns = ['Kans']
+    df = df.sort_values('Kans', ascending=False)
+
+    return df.to_html(formatters={'Kans': '{:,.1%}'.format},
+                      classes=["table table-hover"],
+                      justify="center",
+                      border=0)
